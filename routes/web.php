f1d725be015c931d94264a6738c8a1580bfaefe6
SilasPerillo <silasperillo@gmail.com>
2023-10-09 19:09:35 -0300

    Introduction routes

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -18,5 +19,16 @@ Route::get('/', function () {
 });
 
 Route::get('/hello', function () {
-    return response('<h1>Hello World, First PHP</h1>');
+    return response('<h1>Hello World, First PHP</h1>', 200)
+        ->header('Content-Type', 'text/plain')
+        ->header('foo', 'bar');
+});
+
+Route::get('/posts/{id}', function ($id) {
+    // ddd($id); // para debugar a aplicação
+    return response('Post ' . $id);
+})->where('id', '[0-9]+');
+
+Route::get('/search', function (Request $request) {
+    return $request->name .  ' live in ' . $request->city;
 });
